caecc836dbb9a64db9c37b442c6f21435d3cf4d6
pmbohm <pmbohm@crunchbang.(none)>
2013-10-09 21:15:36 +1100

    missed files

--- convertFolderFiles.py
+++ convertFolderFiles.py
@@ -1,35 +1,41 @@
 #!/usr/bin/python
 import os, os.path, sys, subprocess
+import argparse
+from encoder_functions import encoder_functions
 
-#i = 0
-if (len(sys.argv) > 1):
-	directory = sys.argv[1]
-	directory = directory.strip()
-	if directory[0] == "'" and directory[-1] == "'":
-	    directory = directory[1:-1]
-	print "Checking: " + directory
+#i = 0	
+parser = argparse.ArgumentParser(description='Covert video files matching input type to encoder_functions.py output type in the directory')
+parser.add_argument('directory', help='The folder to find the video files')
+parser.add_argument('-inputFilesExt','--inputFileExt', default="avi", dest="fileInExt", help='The video file extension to convert')
+args = parser.parse_args()
+
+directory = args.directory
+fileInExt = args.fileInExt
+
+if directory[0] == "'" and directory[-1] == "'":
+	directory = directory[1:-1]
+
+if os.path.exists(directory):
+	
+	print "Checking the directory: " + directory
 	for root, dirs, files in os.walk(directory):
-	    print dirs
-	    for f in files:
-		#i += 1
-		if f.endswith('.MOV'):
-			fAsAVI = os.path.splitext(f)[0] + '.avi'
-			fAsAVI = os.path.join(root, fAsAVI)
-			if not os.path.exists(fAsAVI):
-				fullpath = os.path.join(root, f)
-				print "will create " + fAsAVI
-				#p = subprocess.call("ffmpeg -i '" + fullpath + "' -b 3000k -vcodec mjpeg  -flags +loop -cmp +chroma -partitions +parti4x4+partp8x8+partb8x8 -subq 5 -me_range 16 -g 250 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 '" + fAsAVI + "'", shell=True)
-				#p.wait()
-				#os.system("ls -la '" + fullpath + "'")
+		for name in files:
+			if name.endswith(fileInExt):
+				fAsExpected = os.path.splitext(name)[0] + '.mp4'
+				fAsExpected = os.path.join(root, fAsExpected)
+				if not os.path.exists(fAsExpected):
+					theFile = os.path.join(root, name)
+					print "will create " + fAsExpected
+					encoder_functions().ffmpeg(theFile,fAsExpected)
+					
+				else:
+					print "Ignoring existing " + fAsExpected
 			else:
-				print "Ignoring existing " + fAsAVI
-		else:
-			pass
-			#print "ignoreing: " + fullpath
+				pass
+				#print "ignoring: " + name
+
+	print "\n\ndone..\n"
 else:
-    print "\n\n     usage: python convertMOVfiles.py '/media/BigFarger/My Videos/CREATED VIDEOS/'\n"
-
-print "\n\ndone..\n"
-
+	print "\n\n'" + directory + "' cannot be found\n"
 
 
